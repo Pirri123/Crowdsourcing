98778c9c3d0be9f586028a9cc741918cf350d316
Pirri <rs.ernesto1@gmail.com>
2019-04-29 11:17:58 -0500

    Anterior y siguiente en preguntas jala

--- crowdsourcing/app/Http/Controllers/ChallengesController.php
+++ crowdsourcing/app/Http/Controllers/ChallengesController.php
@@ -15,32 +15,43 @@ class ChallengesController extends Controller
         $challenges = DB::table('questions')->where('QuestionType', "1")
             ->join('options', 'options.QuestionId', '=', 'questions.id')
             ->get();
-            $questionIndex = array("First" => $id);
-            //$questionIndex = array("First"=>"0", "Second"=>"1");
-            //echo(sizeof($challenges));
-            //echo($challenges);
-            return view('challenges.index', compact('challenges', 'questionIndex'));
+
+            if ($id < 0){
+                $id = 0;
+            }
+            if ($id % 2 == 1){
+                $id = $id - 1;
+            }
+            return view('challenges.index', compact('challenges', 'id'));
     }
 
     public function textImageIndex($id){
         $challenges = DB::table('questions')->where('QuestionType', "2")
             ->join('options', 'options.QuestionId', '=', 'questions.id')
             ->get();
-            $questionIndex = array("First" => $id);
-           
-            //echo(sizeof($challenges));
-            echo($challenges);
-            return view('challenges.index', compact('challenges', 'questionIndex'));
+
+            if ($id < 0){
+                $id = 0;
+            }
+            if ($id % 2 == 1){
+                $id = $id - 1;
+            }
+
+            return view('challenges.index', compact('challenges', 'id'));
     }
 
     public function imageImageIndex($id){
         $challenges = DB::table('questions')->where('QuestionType', "3")
             ->join('options', 'options.QuestionId', '=', 'questions.id')
             ->get();
-            $questionIndex = array("First" => $id);
-            //echo(sizeof($challenges));
-            //echo($challenges);
-            return view('challenges.index', compact('challenges', 'questionIndex'));
+
+            if ($id < 0){
+                $id = 0;
+            }
+            if ($id % 2 == 1){
+                $id = $id - 1;
+            }
+            return view('challenges.index', compact('challenges', 'id'));
     }
 
     public function create(){
--- crowdsourcing/resources/views/challenges/index.blade.php
+++ crowdsourcing/resources/views/challenges/index.blade.php
@@ -5,18 +5,26 @@
   <title>Challenge!</title>
 </head>
 <body>
-    <h1> {{$challenges[$questionIndex["First"]]->QuestionText}} </h1>
-   
-    <!--<h1> Accounts</h1>-->
-    <img src = {{asset("img/questions/" . $challenges[$questionIndex["First"]]->ImgLocation)}}
-    <br>
-    <img src = {{asset("img/questions/" . $challenges[$questionIndex["First"] +1]->ImgLocation)}}
-   
-    <div>
-      
-        <button href="{{ url()->previous() }}" class="btn btn-default">Back</button>
-        <a href="{{ url()->previous() }}" class="btn btn-default">Back</a>
+    @if($id == count($challenges))
+    <h1> Ya no hay más preguntas, vuelva más tarde! </h1>
+    @else
+        <h1> {{$challenges[$id]->QuestionText}} </h1>
+    
+        <!--<h1> Accounts</h1>-->
+        <img src = {{asset("img/questions/" . $challenges[$id]->ImgLocation)}}
+        <br>
+        <img src = {{asset("img/questions/" . $challenges[$id+1]->ImgLocation)}}
+    
+        <div>
+        <br>
 
+        @if($id >= 2)
+        <a href="/{{Request::segment(1)}}/{{$id-2}}"><button type="button" class="btn btn-xs btn-info" >Anterior</button></a>
+        @endif
+        @if($id < (count($challenges) - 1))
+        <a href="/{{Request::segment(1)}}/{{$id+2}}"><button type="button" class="btn btn-xs btn-info" >Siguiente</button></a>
+        @endif
+    @endif
       
     </div>
 </body>
